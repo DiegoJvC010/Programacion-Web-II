15097fab1c3b74330e63ee5928e82b0df9b6489b
Diego Jesus <s21120187@alumnos.itsur.edu.mx>
2024-09-14 17:07:26 -0600

    Personalice la lista de cambios del sitio administrativo - Modificacion lista de cambios de Questions y adicion del metodo was_published_recently

--- polls/admin.py
+++ polls/admin.py
@@ -12,6 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
