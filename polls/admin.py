db3b1070712416ee165339dc3b81ff33657ebe2e
Diego Jesus <s21120187@alumnos.itsur.edu.mx>
2024-09-14 16:09:28 -0600

    Personalice el formulario del sitio administrativo - Cambio primero date despues Question y ajuste para que aparezca el texto de Question

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from polls.models import Question, Choice
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
